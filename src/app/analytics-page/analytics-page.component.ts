2324b0ba613f21077f5981bb492c0387e2dfde30
h-daria <dariahalaida@gmail.com>
2022-07-12 19:00:34 +0300

    added analytics page header and sidebar

---
+++ src/app/analytics-page/analytics-page.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-analytics-page',
+  templateUrl: './analytics-page.component.html',
+  styleUrls: ['./analytics-page.component.css']
+})
+export class AnalyticsPageComponent implements OnInit {
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+}
