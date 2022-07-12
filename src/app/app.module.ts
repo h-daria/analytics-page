4c4412267ca8439ba0efd028289c5adba086ad46
h-daria <dariahalaida@gmail.com>
2022-07-12 18:58:55 +0300

    project organisational changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AnalyticsPageComponent } from './analytics-page/analytics-page.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
+import { SidebarMenuComponent } from './sidebar-menu/sidebar-menu.component';
+import { CryptocurrencyComponent } from './cryptocurrency/cryptocurrency.component';
+import { CryptocurrencyInfoComponent } from './cryptocurrency-info/cryptocurrency-info.component';
+import { AdditionalInfoComponent } from './additional-info/additional-info.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnalyticsPageComponent,
+    SidebarMenuComponent,
+    CryptocurrencyComponent,
+    CryptocurrencyInfoComponent,
+    AdditionalInfoComponent
   ],
   imports: [
+    FormsModule,
+    ReactiveFormsModule,
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
